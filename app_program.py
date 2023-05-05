de67d927a50342523347e0ca571f0919e1d2ffcc
copper-head <dpt33c@umsystem.edu>
2023-05-04 19:06:56 -0500

    Pushing app program

--- app_program.py
+++ app_program.py
@@ -6,7 +6,7 @@ API_BASE_URL = "http://localhost:5000"
 head = {"Content-Type":"application/json"}
 
 user_token = None
-username = None
+g_user_name = None
 first_name = None
 last_name = None
 
@@ -46,9 +46,13 @@ def login():
         if response_info["TYPE"] == "LoginError":
             print(response_info["MESSAGE"])
         elif response_info["TYPE"] == "LoginSuccess":
-            print("Login Successful!")
+            print("\nLogin Successful!")
+            global user_token
+            global g_user_name
+            g_user_name = username
             user_token = response_info["TOKEN"]
             input_valid = True
+            main_menu()
     
 def create_account():
 
@@ -88,11 +92,12 @@ def create_account():
 
 def main_menu():
     
+    input_valid = False
     exit = False
     while not input_valid:
 
-        print("Select an action: ")
-        print("1 - View Welfare Porgrams")
+        print("\nSelect an action: ")
+        print("1 - View Welfare Programs")
         print("2 - View Submitted Applications")
         print("3 - Submit New Application")
         print("4 - Exit")
@@ -101,12 +106,16 @@ def main_menu():
         print()
 
         if user_input == "1":
+            input_valid = True
             view_welfare_programs()
         elif user_input == "2":
+            input_valid = True
             view_submitted_applications()
         elif user_input == "3":
+            input_valid = True
             submit_new_application()
         elif user_input == "4":
+            input_valid = True
             print("GOODBYE!")
             exit()
         else:
@@ -118,13 +127,42 @@ def main_menu():
             
 
 def view_welfare_programs():
-    pass
+    print('Press Enter to exit to the menu\n')
+
+    url = "http://localhost:5000/api/get/welfare_programs"
+    response = requests.request("GET", url, headers=head)
+   
+    welfares = json.loads(response.text)
+    print('{:<20}'.format('Name')+'{:<20}'.format('Type') + 'Available Tiers')
+    print('{:-<55}'.format('-'))
+    for i in welfares["payload"]:
+        for j  in i:
+                print('{:<20}'.format(j), end='')
+        print()
+
+    input()
+    main_menu()
+
 
 def view_submitted_applications():
-    pass
+
+    url = API_BASE_URL + "/api/applicant/view_applications"
+    print(user_token)
+    payload = json.dumps({"username": g_user_name,"token": user_token})
+    response = requests.request("POST", url, headers=head, data=payload)
+    print(type(response.text))
+    print(response.text)
 
 def submit_new_application():
-    pass
+    appName = ''
+    valid = False
+    while not valid:
+        appName = input("Choose a welfare program to apply for ('q' to quit): ")
+        #FIXME: check if appName is valid
+        #if valid, give confirmation message and exit
+        if(appName == 'q'):
+            valid = True
+    main_menu()
 
 if __name__ == '__main__':
     welcome()
